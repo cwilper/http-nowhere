134b63d794590fa6fe5f79f785e7690bf739225b
Chris Wilper <cwilper@gmail.com>
2013-08-09 09:01:40 -0400

    button work

--- firefox/src/chrome/content/button.js
+++ firefox/src/chrome/content/button.js
@@ -6,13 +6,6 @@ if ("undefined" === typeof(httpNowhere)) var httpNowhere = {};
 
 httpNowhere.button = {
 
-  recentlyBlocked: new Array(),
-
-  clearRecentList: function() {
-    httpNowhere.button.recentlyBlocked = new Array();
-    httpNowhere.button.updateView();
-  },
-
   init: function() {
     // Add the button, update the view, and show the installed message, if needed
     setTimeout(function() {
@@ -32,10 +25,10 @@ httpNowhere.button = {
            document.persist('nav-bar', 'currentset');
         }
         // give a quick one-time usage message
-        Services.prompt.alert(null, "HTTP Nowhere is now installed", "Click the lock button in the toolbar to enable or disable it.\n\nWhile enabled, unencrypted web requests will fail.");
+        Services.prompt.alert(null, "HTTP Nowhere is now installed", "Click the lock button to enable or disable it.\n\nWhile enabled, unencrypted web requests will fail.");
         httpNowhere.prefs.setFirstRun(false);
       }
-      httpNowhere.button.updateView();
+      httpNowhere.button.updateButtonAppearance();
     }, 500);
 
     // Observe interesting stuff
@@ -46,69 +39,90 @@ httpNowhere.button = {
     if (topic == "http-on-modify-request" && httpNowhere.prefs.isEnabled()) {
       var request = subject.QueryInterface(Ci.nsIHttpChannel);
       if (request.URI.scheme == "http" && request.URI.host != 'localhost') {
-        var notifyImage = "chrome://http-nowhere/skin/button-notify.png";
+        var blockingImage = "chrome://http-nowhere/skin/button-blocking.png";
         // signal that a block has occurred by briefly changing the button
         var button = document.getElementById("http-nowhere-button");
         if (button != null) {
-          if (button.image != notifyImage) {
-            button.image = notifyImage;
+          if (button.image != blockingImage) {
+            button.image = blockingImage;
             setTimeout(function() {
-              httpNowhere.button.updateView();
-            }, 500);
+              httpNowhere.button.updateButtonAppearance();
+            }, 400);
           }
         }
         // abort the request
         request.cancel(Components.results.NS_ERROR_ABORT);
         // update the recent list
         httpNowhere.recent.addURI(request.URI);
-        // TODO: remove...this is the old way
-        httpNowhere.button.recentlyBlocked.push(request.URI.spec);
-        if (httpNowhere.button.recentlyBlocked.length > 20) {
-          httpNowhere.button.recentlyBlocked.pop();
-        }
-        // TODO: don't do this now...do it lazily on popup
-        if (button.image != notifyImage) {
-          httpNowhere.button.updateView();
-        }
       }
     }
   },
 
-  updateView: function() {
+  updateButtonAppearance: function() {
     var button = document.getElementById("http-nowhere-button");
     if (button != null) {
-      var toggle = document.getElementById("http-nowhere-toggle");
+      // TODO: get urls from dtd
       var onImage = "chrome://http-nowhere/skin/button-on.png";
+      var blockedImage = "chrome://http-nowhere/skin/button-blocked-19.png";
       var offImage = "chrome://http-nowhere/skin/button-off.png";
       if (httpNowhere.prefs.isEnabled()) {
-        button.image = onImage;
+        if (httpNowhere.recent.blockCount == 0) {
+          button.image = onImage;
+        } else {
+          button.image = blockedImage;
+        }
         button.tooltipText = "HTTP Nowhere (Enabled)";
-        toggle.image = offImage;
-        toggle.label = "Disable HTTP Nowhere";
       } else {
         button.image = offImage;
         button.tooltipText = "HTTP Nowhere (Disabled)";
-        toggle.image = onImage;
-        toggle.label = "Enable HTTP Nowhere";
-      }
-      var recentlyBlocked = document.getElementById("http-nowhere-recently-blocked");
-      recentlyBlocked.label = "Recently Blocked (" + httpNowhere.button.recentlyBlocked.length + ")";
-      var recentlyBlockedPopup = document.getElementById("http-nowhere-recently-blocked-popup");
-      while (recentlyBlockedPopup.firstChild.tagName != "menuseparator") {
-        recentlyBlockedPopup.removeChild(recentlyBlockedPopup.firstChild);
-      }
-      for (var i = 0; i < httpNowhere.button.recentlyBlocked.length; i++) {
-        var menuitem = document.createElement("menuitem");
-        menuitem.setAttribute('label', httpNowhere.button.recentlyBlocked[i]);
-        recentlyBlockedPopup.insertBefore(menuitem, recentlyBlockedPopup.firstChild);
       }
     }
   },
 
-  toggleState: function() {
-    httpNowhere.prefs.setEnabled(!httpNowhere.prefs.isEnabled());
-    httpNowhere.button.updateView();
+  updateTopMenu: function() {
+    // TODO: get urls from dtd
+    var onImage = "chrome://http-nowhere/skin/button-on.png";
+    var offImage = "chrome://http-nowhere/skin/button-off.png";
+    var toggle = document.getElementById("http-nowhere-toggle");
+    if (httpNowhere.prefs.isEnabled()) {
+      toggle.image = offImage;
+      toggle.label = "Disable HTTP Nowhere";
+    } else {
+      toggle.image = onImage;
+      toggle.label = "Enable HTTP Nowhere";
+    }
+
+    var recentlyBlocked = document.getElementById("http-nowhere-recently-blocked");
+    recentlyBlocked.label = "Recent Blocks (" + httpNowhere.recent.blockCount + ")";
   },
+
+  updateRecentMenu: function() {
+    var recentlyBlockedPopup = document.getElementById("http-nowhere-recently-blocked-popup");
+    while (recentlyBlockedPopup.firstChild.tagName != "menuseparator") {
+      recentlyBlockedPopup.removeChild(recentlyBlockedPopup.firstChild);
+    }
+
+    var orderedHostnames = httpNowhere.recent.getKeysOrderedByLastBlockedDate(httpNowhere.recent.hostInfo);
+    Services.console.logStringMessage(orderedHostnames);
+    for (var i = 0; i < orderedHostnames.length; i++) {
+      var hostname = orderedHostnames[i];
+      var hostInfo = httpNowhere.recent.hostInfo[orderedHostnames[i]];
+      Services.console.logStringMessage("hostInfo[" + hostname + "]: " + JSON.stringify(hostInfo));
+      var menuitem = document.createElement("menuitem");
+      menuitem.setAttribute('label', hostname + " (" + hostInfo.blockCount + ")");
+      recentlyBlockedPopup.insertBefore(menuitem, recentlyBlockedPopup.firstChild);
+    }
+  },
+
+  toggle: function() {
+    httpNowhere.prefs.setEnabled(!httpNowhere.prefs.isEnabled());
+    httpNowhere.button.updateButtonAppearance();
+  },
+
+  clearRecent: function() {
+    httpNowhere.recent.clear();
+    httpNowhere.button.updateButtonAppearance();
+  }
 }
 
 window.addEventListener("load", httpNowhere.button.init, false);
--- firefox/src/chrome/content/recent.js
+++ firefox/src/chrome/content/recent.js
@@ -9,50 +9,64 @@ httpNowhere.recent = {
 
   hostInfo: {},
 
+  blockCount: 0,
+
   clear: function() {
     httpNowhere.recent.hostInfo = {};
+    httpNowhere.recent.blockCount = 0;
   },
 
   addURI: function(uri) {
     var now = new Date().getTime();
 
+    // update hostInfo as appropriate
     var hostInfo = httpNowhere.recent.hostInfo[uri.host];
     if (hostInfo == null) {
       // add new host to the recent hostInfo list
       hostInfo = {
         blockCount: 0,
-        hostname: uri.host,
         urlInfo: {}
       };
       httpNowhere.recent.hostInfo[uri.host] = hostInfo;
       if (Object.keys(httpNowhere.recent.hostInfo).length > httpNowhere.prefs.getMaxRecentlyBlockedHosts()) {
-        // delete the host with the oldest blocked date
-        // TODO: implement me
+        // delete the hostInfo with the oldest blocked date
+        var orderedHostnames = httpNowhere.recent.getKeysOrderedByLastBlockedDate(httpNowhere.recent.hostInfo);
+        delete httpNowhere.recent.hostInfo[orderedHostnames[orderedHostnames.length - 1]];
       }
     }
     hostInfo.blockCount += 1;
     hostInfo.lastBlockedDate = now;
 
-    // add the url to the list of those blocked for this host
+    // update hostInfo.urlInfo as appropriate
     var urlInfo = hostInfo.urlInfo[uri.spec];
     if (urlInfo == null) {
-      // add new url to the urlInfo for this host
       urlInfo = {
         blockCount: 0
       };
       hostInfo.urlInfo[uri.spec] = urlInfo;
       if (Object.keys(hostInfo.urlInfo) > httpNowhere.prefs.getMaxRecentlyBlockedURLsPerHost()) {
-        // delete the url with the oldest blocked date
-        // TODO: implement me
+        // delete the urlInfo with the oldest blocked date
+        var orderedUrls = httpNowhere.recent.getKeysOrderedByLastBlockedDate(hostInfo.urlInfo);
+        var oldUrlInfo = hostInfo.urlInfo[orderedUrls[orderedUrls.length - 1]];
+        delete hostInfo.urlInfo[orderedUrls[orderedUrls.length - 1]];
+        // recalculate the block count for this hostInfo
+        hostInfo.blockCount -= oldUrlInfo.blockCount;
       }
     }
     urlInfo.blockCount += 1;
     urlInfo.lastBlockedDate = now;
+
+    // recalculate the global block count
+    var blockCount = 0;
+    for (var hostname in httpNowhere.recent.hostInfo) {
+      blockCount += httpNowhere.recent.hostInfo[hostname].blockCount;
+    }
+    httpNowhere.recent.blockCount = blockCount;
   },
 
-  getKeysSortedByDate: function(info) {
+  getKeysOrderedByLastBlockedDate: function(info) {
     return Object.keys(info).sort(function(a, b) {
       return info[a].lastBlockedDate - info[b].lastBlockedDate;
     });
-  }
+  },
 }
