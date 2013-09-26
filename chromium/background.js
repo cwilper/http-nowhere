6abb71725ac77f783397924a09b8ea7b77582683
Chris Wilper <cwilper@gmail.com>
2013-09-26 09:48:43 -0400

    code style: define .recent, .prefs, etc outside the main block

--- chromium/background.js
+++ chromium/background.js
@@ -30,34 +30,36 @@ var httpNowhere = {
         chrome.tabs.create({url: pageUrl});
       }
     });
-  },
+  }
+};
 
-  button: {
-    updateAppearance: function() {
-      chrome.browserAction.setBadgeBackgroundColor({'color':'#999'});
-      if (httpNowhere.recent.blockCount > 0) {
-        chrome.browserAction.setBadgeText({'text': httpNowhere.recent.blockCount + ''});
-      } else {
-        chrome.browserAction.setBadgeText({'text': ''});
-      }
+httpNowhere.button = {
+  updateAppearance: function() {
+    chrome.browserAction.setBadgeBackgroundColor({'color':'#999'});
+    if (httpNowhere.recent.blockCount > 0) {
+      chrome.browserAction.setBadgeText({'text': httpNowhere.recent.blockCount + ''});
+    } else {
+      chrome.browserAction.setBadgeText({'text': ''});
     }
+  }
+};
+
+httpNowhere.prefs = {
+  _enabled: true,
+
+  isEnabled: function() {
+    return httpNowhere.prefs._enabled;
   },
 
-  prefs: {
-    _enabled: true,
+  // https://chrome.google.com/webstore/detail/storage-area-explorer/ocfjjjjhkpapocigimmppepjgfdecjkb
+  setEnabled: function(value) {
+    console.log('setEnabled(' + value + ')');
+    httpNowhere.prefs._enabled = value;
+    chrome.storage.local.set({'enabled': value});
+  }
+};
 
-    isEnabled: function() {
-      return httpNowhere.prefs._enabled;
-    },
-
-    // https://chrome.google.com/webstore/detail/storage-area-explorer/ocfjjjjhkpapocigimmppepjgfdecjkb
-    setEnabled: function(value) {
-      console.log('setEnabled(' + value + ')');
-      httpNowhere.prefs._enabled = value;
-      chrome.storage.local.set({'enabled': value});
-    }
-  },
-
+httpNowhere.recent = {
   recent: {
     blockCount: 0
   }
