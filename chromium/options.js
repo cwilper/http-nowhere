a79171082f46f6d77ee525a0f8d10ba39ab58e7d
Chris Wilper <cwilper@gmail.com>
2013-09-29 23:18:05 -0400

    start list editing control

--- chromium/options.js
+++ chromium/options.js
@@ -10,13 +10,49 @@ function dialogOpened() {
   for (var i = 0; i < menuitems.length; i++) {
     menuitems[i].addEventListener('click', function(e) { pageSelected(e.target); });
   }
-  var closeButtons = document.getElementsByClassName('closeButton');
-  for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', function() { window.close(); });
+  var doneButtons = document.getElementsByClassName('doneButton');
+  for (var i = 0; i < doneButtons.length; i++) {
+    doneButtons[i].addEventListener('click', function() { window.close(); });
+  }
+  var addButtons = document.getElementsByClassName('addButton');
+  for (var i = 0; i < addButtons.length; i++) {
+    addButtons[i].addEventListener('click', function(e) {
+      clickedAdd(e.target.getAttribute('list'));
+    });
+  }
+  var editButtons = document.getElementsByClassName('editButton');
+  for (var i = 0; i < editButtons.length; i++) {
+    editButtons[i].addEventListener('click', function(e) {
+      clickedEdit(e.target.getAttribute('list'));
+    });
+  }
+  var deleteButtons = document.getElementsByClassName('deleteButton');
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener('click', function(e) {
+      clickedDelete(e.target.getAttribute('list'));
+    });
+  }
+  var urllists = document.getElementsByClassName('urllist');
+  for (var i = 0; i < urllists.length; i++) {
+    urllists[i].addEventListener('click', function() {
+      if (this.selectedIndex > -1) {
+        setButtonDisabled('edit', this.getAttribute('list'), false);
+        setButtonDisabled('delete', this.getAttribute('list'), false);
+      }
+    });
   }
 
   // make initial selection
   pageSelected(document.getElementById('general'));
+}
+
+function setButtonDisabled(type, list, disabled) {
+  var buttons = document.getElementsByClassName(type + 'Button');
+  for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].getAttribute('list') == list) {
+      buttons[i].disabled = disabled;
+    }
+  }
 }
 
 function pageSelected(e) {
@@ -28,7 +64,7 @@ function pageSelected(e) {
   }
   e.className = 'menuitem menuitem-selected';
 
-  // activate the associated pane
+  // initialize and show the associated pane
   var panes = document.getElementsByClassName('options-pane');
   for (var i = 0; i < panes.length; i++) {
     panes[i].setAttribute('class', 'options-pane');
@@ -72,9 +108,26 @@ function initBooleanControl(id, getter, setter) {
   });
 }
 
-function generalPageLoaded() {
-  debug('generalPageLoaded()');
+function initUrlPatternListControl(list, patterns) {
+  // set initial view state from data
+  var select = document.getElementById(list + 'List');
+  while (select.hasChildNodes()) {
+    select.removeChild(select.firstChild);
+  }
+  patterns.sort(function(a, b){return a < b ? -1 : 1});
+  for (var i = 0; i < patterns.length; i++) {
+    var option = document.createElement('option');
+    option.setAttribute('value', patterns[i]);
+    option.appendChild(document.createTextNode(patterns[i]));
+    select.appendChild(option);
+  }
 
+  // no initial selection so edit and delete buttons are disabled
+  setButtonDisabled('edit', list, true);
+  setButtonDisabled('delete', list, true);
+}
+
+function generalPageLoaded() {
   // set values from prefs and attach event listeners to handle changes
   initIntegerControl('maxRecentlyBlockedHosts', 0, 99,
       httpNowhere.prefs.getMaxRecentlyBlockedHosts,
@@ -95,10 +148,24 @@ function generalPageLoaded() {
 
 function allowedPageLoaded() {
   debug('allowedPageLoaded()');
+  initUrlPatternListControl('allowed', httpNowhere.rules.allowedPatterns);
 }
 
 function ignoredPageLoaded() {
   debug('ignoredPageLoaded()');
+  initUrlPatternListControl('ignored', httpNowhere.rules.ignoredPatterns);
+}
+
+function clickedAdd(list) {
+  debug('clickedAdd(' + list + ')');
+}
+
+function clickedEdit(list) {
+  debug('clickedEdit(' + list + ')');
+}
+
+function clickedDelete(list) {
+  debug('clickedDelete(' + list + ')');
 }
 
 function debug(message) {
